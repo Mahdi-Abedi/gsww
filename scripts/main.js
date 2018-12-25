48dcfb059fc09cfb0c4557613cf74988b4e6d1be
Mahdi Abedi <abedi_mahdi@ymail.com>
2018-12-25 19:31:00 +0330

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+﻿// Image switcher code
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/Faranesh-image.png') {
+      myImage.setAttribute ('src','images/Faranesh-image-2.jpg');
+	} else {
+	  myImage.setAttribute ('src','images/Faranesh-image.png');
+	}
+}
+
+// Personalized welcome message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('لطفاً نام خود را وارد کنید:');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'سلام ' + myName + ',خوش آمدید';
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'سلام ' + storedName + ',خوش آمدید';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
